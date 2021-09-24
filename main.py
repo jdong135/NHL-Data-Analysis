ac89b5d72693fb4f576497dd8654f130411a136e
Jay Dong <jdong@caltech.edu>
2021-09-24 11:36:50 -0700

    Cleaned up Players class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import player_ids
 
 player = player_ids.Players()
+player.get_id_map()
 
 # df = pd.read_pickle('scraped_info/player_id_dataframe.pkl')
--- player_ids.py
+++ player_ids.py
@@ -4,30 +4,42 @@ import requests
 import json
 import pandas as pd
 
-# Map of {player: id number}
-class Players:
-    def __init__(self):
-        self.id_map = {}
 
-    def get_id_map(self):
-        # If scraped_info dataframe doesn't exist, run scrape
+class Players:
+    """
+    A Players object contains functionality to create a dataframe of
+    player names and their IDs. The dataframe is stored in a shared file in a
+    different directory.
+    """
+
+    def update_id_map(self):
+        """
+        Params: none
+        Returns: none
+        Check if a dataframe already exists. Update it if it's been more than
+        14 days since last update. 
+        """
         file_exists = os.path.exists('scraped_info/player_id_dataframe.pkl')
         if file_exists:
             # Check when the df was last updated
             last_updated_epoch = os.path.getmtime('scraped_info/player_id_dataframe.pkl')
-            last_updated_date = datetime.datetime.fromtimestamp(last_updated_epoch)
+            last_updated_date = datetime.date.fromtimestamp(last_updated_epoch)
             cur_date = datetime.date.today()
             time_since_update = cur_date - last_updated_date
             if time_since_update.days > 14:
-                # Scrape again
-                self.scrape()
-            else:
-                # Return existing dataframe
-                return
+                self.populate_id_map()
         else:
-            self.scrape()
+            self.populate_id_map()
+    
     # Create dataframe with one column as player, one column as ID
-    def scrape(self):
+    def populate_id_map(self):
+        """
+        Params: none
+        Returns: none
+        Create a dataframe where one column is all the current NHL players'
+        names and the other column is his corresponding ID. 
+        """
+        id_map = {}
         team_ids = self.get_team_ids()
         for id in team_ids:
             url = 'https://statsapi.web.nhl.com/api/v1/teams/{}?expand=team.roster'.format(id)
@@ -36,12 +48,15 @@ class Players:
             for player in roster:
                 name = player['person']['fullName']
                 id = player['person']['id']
-                self.id_map[name] = id
-        df = pd.DataFrame(self.id_map.items(), columns=['Name', 'ID'])
+                id_map[name] = id
+        df = pd.DataFrame(id_map.items(), columns=['Name', 'ID'])
         df.to_pickle('scraped_info/player_id_dataframe.pkl')
-        return self.id_map
 
     def get_team_ids(self):
+        """
+        Params: none
+        Returns: list of team IDs
+        """
         ids = []
         response = requests.get('https://statsapi.web.nhl.com/api/v1/teams').json()
         teams = response['teams']
